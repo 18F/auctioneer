ff0f582fc4d67af5eee0e1c3347a561364b0b07d
Alan deLevie <adelevie@gmail.com>
2016-02-24 14:16:33 -0500

    Added clear command

--- auctioneer.rb
+++ auctioneer.rb
@@ -117,6 +117,7 @@ def monitor_live_auctions
         bids << Auctioneer::Bid.new(bid, auction)
       end
     end
+    system "clear" or system "cls"
     STDOUT.puts "Bids so far:"
     tp bids, :bidder_name, :amount, {auction_title: {width: 50}}, {auction_url: {width: 50}}, :bidder_github_id, :bidder_duns_number
     sleep 10
